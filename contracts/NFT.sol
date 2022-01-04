774e55e014e55abde0e9cd333114c2a3018e9804
nayeem01 <nayeemabdullah00@gmail.com>
2022-01-04 15:23:44 +0600

    nft added

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -3,7 +3,57 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "base64-sol/base64.sol";
 
 contract NFT is ERC721URIStorage {
-    //constructor() {}
+    using Counters for Counters.Counter;
+    Counters.Counter private tokenId;
+    event createdNFT(uint256 indexed _tokenId, string url);
+
+    constructor() ERC721("NFT", "imgNFT") {}
+
+    function createNFT(string memory _url) public {
+        tokenId.increment();
+        uint256 newTokenID = tokenId.current();
+
+        _safeMint(msg.sender, newTokenID);
+        string memory img = imgToURL(_url);
+
+        _setTokenURI(newTokenID, jsonResponse(img));
+
+        emit createdNFT(newTokenID, _url);
+    }
+
+    function imgToURL(string memory _url) public pure returns (string memory) {
+        string memory baseURL = "data:image/jpg;base64,";
+        string memory encodedUrl = Base64.encode(
+            bytes(string(abi.encodePacked(_url)))
+        );
+        return string(abi.encodePacked(baseURL, encodedUrl));
+    }
+
+    function jsonResponse(string memory imageURI)
+        public
+        pure
+        returns (string memory)
+    {
+        return
+            string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"name":"',
+                                "NFT",
+                                '", "description":"", "attributes":"", "image":"',
+                                imageURI,
+                                '"}'
+                            )
+                        )
+                    )
+                )
+            );
+    }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,38 @@
 require("@nomiclabs/hardhat-waffle");
+require("hardhat-deploy");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+    const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
+    for (const account of accounts) {
+        console.log(account.address);
+    }
 });
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: { gas: 10000000 },
+        localhost: {
+            url: "http://127.0.0.1:8545",
+        },
+    },
+    solidity: "0.8.4",
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+    },
+    paths: {
+        sources: "./contracts",
+        tests: "./test",
+        cache: "./cache",
+        artifacts: "./artifacts",
+    },
+    mocha: {
+        timeout: 20000,
+    },
 };
